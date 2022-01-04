9f6a83be10b7dd074bee68966aeec63329094cc8
tn-teamrg <85945974+tn-teamrg@users.noreply.github.com>
2022-01-04 16:38:42 -0500

    isolating tests by introducing setup initializations

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -2,22 +2,31 @@ import {CalculatorService} from './calculator.service';
 import {LoggerService} from './logger.service';
 
 describe('CalculatorService', () => {
+  let calculator: CalculatorService,
+    // NOTE: loggerSpy can return a variety of values
+    loggerSpy: any;
+
+  beforeEach(() => {
+    console.log('calling before');
+
+    loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+    calculator = new CalculatorService(loggerSpy);
+  });
 
   it('should add two numbers', () => {
+    console.log('add test');
 
-    // NOTE: LoggerService.log() to using fake objects
-    const logger = jasmine.createSpyObj('LoggerService', ['log']);
-
-    const calculator = new CalculatorService(logger);
     const result = calculator.add(2, 2);
     expect(result).toBe(4, 'Unexpected addition result');
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
   it('should subtract two numbers', () => {
-    const calculator = new CalculatorService(new LoggerService());
+    console.log('subtract test');
+
     const result = calculator.subtract(2, 2);
     expect(result).toBe(0, 'Unexpected subtraction result');
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
 });
